9259f7438458c275ead7eb9ee696a785023ba0e9
bhlindemann <bh@lindemannrock.com>
2025-09-24 23:09:13 +0400

    fix: Enable log viewer for Translation Manager on Servd integration

--- src/TranslationManager.php
+++ src/TranslationManager.php
@@ -89,7 +89,7 @@ class TranslationManager extends Plugin
             'pluginHandle' => $this->handle,
             'pluginName' => $this->name,
             'logLevel' => $logLevel,
-            'enableLogViewer' => !isset($_ENV['SERVD_PROJECT_SLUG']), // Disable on Servd - they provide log viewing
+            'enableLogViewer' => true, // Keep file logging enabled - Servd will auto-capture from /storage/logs
             'permissions' => ['translationManager:viewTranslations'],
         ]);
 
