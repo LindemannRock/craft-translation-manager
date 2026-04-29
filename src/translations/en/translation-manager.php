578052f5d1be91010d3fb4ec1ff5252a64d3da10
bhlindemann <bh@lindemannrock.com>
2026-04-29 22:39:39 +0400

    fix: update copyright year in translation-manager.php

--- src/translations/en/translation-manager.php
+++ src/translations/en/translation-manager.php
@@ -3,7 +3,7 @@
  * Translation Manager plugin for Craft CMS 5.x
  *
  * @link      https://lindemannrock.com
- * @copyright Copyright (c) 2025 LindemannRock
+ * @copyright Copyright (c) 2026 LindemannRock
  */
 
 return [
