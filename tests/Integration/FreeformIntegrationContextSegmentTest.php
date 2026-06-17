6fc9b13b55391e94970631e34c4aebb4696d5a91
bhlindemann <bh@lindemannrock.com>
2026-06-17 19:26:36 +0400

    feat(tests): add FreeformIntegration context segment test

---
+++ tests/Integration/FreeformIntegrationContextSegmentTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * LindemannRock Translation Manager
+ *
+ * @link      https://lindemannrock.com
+ * @copyright Copyright (c) 2026 LindemannRock
+ */
+
+declare(strict_types=1);
+
+namespace lindemannrock\translationmanager\tests\Integration;
+
+use lindemannrock\translationmanager\integrations\FreeformIntegration;
+use lindemannrock\translationmanager\tests\TestCase;
+
+/**
+ * @since 5.26.0
+ */
+final class FreeformIntegrationContextSegmentTest extends TestCase
+{
+    public function testContextSegmentHashesOriginalValueWhenSanitizedValueIsEmpty(): void
+    {
+        $integration = new FreeformIntegration();
+        $method = new \ReflectionMethod($integration, 'contextSegment');
+
+        self::assertSame(substr(md5('***'), 0, 8), $method->invoke($integration, '***'));
+        self::assertSame(substr(md5('###'), 0, 8), $method->invoke($integration, '###'));
+        self::assertNotSame($method->invoke($integration, '***'), $method->invoke($integration, '###'));
+    }
+}
